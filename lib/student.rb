80f79350474390b33dc8153e8df7d3cd2c7aa52b
James Rogers <jarogers095@gmail.com>
2020-02-17 06:37:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+require "./lib/user.rb"
+class Student < User
+  attr_reader(:knowledge)
+  
+  def initialize()
+    @knowledge = []
+  end
+  
+  def learn(lesson)
+    @knowledge << lesson
+  end
 end
