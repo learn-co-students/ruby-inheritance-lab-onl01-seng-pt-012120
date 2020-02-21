e391666594ce1557c5b97f34ae607f72bf93e65d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-21 14:59:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge #takes in arg and adds to knowledge array 
+  end
+
+  def knowledge #this is returning the knowledge array
+    @knowledge
+  end
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,10 +2,14 @@ class User
 
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
+  #Note to self:
+  #the attr_accessor is the getter and setter method
+  #we don't need to initialize here because we don't have instances
 
-  
+  #def initialize(first_name, last_name)
+    #@first_name = first_name
+    #@last_name = last_name
+  #end
+
+
 end
